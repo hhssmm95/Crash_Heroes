7c963a2d0d07bf83d23e8d548c781985343e12f4
hhssmm95 <42854702+hhssmm95@users.noreply.github.com>
2020-04-21 19:55:33 +0900

    WarriorSkill1
    임시 전사스킬1 동작추가 Z키

--- Assets/Scripts/WarriorSkill.cs
+++ Assets/Scripts/WarriorSkill.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class WarriorSkill : MonoBehaviour
 {
-    // Start is called before the first frame update
+    CharacterMove player;
+    Animator playerAnim;
+    public float slash_Cooltime = 1.5f;
+    float slash_Timer = 0;
+    bool slashOff;
     void Start()
     {
-        
+        player = gameObject.GetComponent<CharacterMove>();
+        playerAnim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Skill_Slash();
+
+        if(slashOff)
+        {
+            slash_Timer += Time.deltaTime;
+            if (slash_Timer >= slash_Cooltime)
+            {
+                slashOff = false;
+                slash_Timer = 0;
+            }
+        }
+    }
+
+    void Skill_Slash()
+    {
+        if (Input.GetKeyDown(KeyCode.Z) && !slashOff)
+        {
+            slashOff = true;
+            playerAnim.SetTrigger("attack_1");
+        }
     }
 }
