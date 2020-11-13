840e774e6931add7cf3b85896174f82113426f4a
Clarias <20142315@vision.hoseo.edu>
2020-11-13 21:47:04 +0900

    new algorithm on Mage Skills

---
+++ Assets/Scripts/ParticleDest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDest : MonoBehaviour
+{
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, timer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
